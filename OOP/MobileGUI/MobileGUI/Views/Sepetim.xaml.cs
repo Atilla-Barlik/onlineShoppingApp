e07eb17130d3b36efa8ed9e62d00f0471b779b1c
unknown <gurkanozgel58@gmail.com>
2022-04-25 23:04:33 +0300

    Kod düzenlemeleri yapıldı

--- OOP/MobileGUI/MobileGUI/Views/Sepetim.xaml.cs
+++ OOP/MobileGUI/MobileGUI/Views/Sepetim.xaml.cs
@@ -15,35 +15,13 @@ namespace MobileGUI.Views
     [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class Sepetim : ContentPage
     {
-        private string imageSource;
-        private string productName;
-        private string price;
+        
         public Sepetim()
         {
             InitializeComponent();
             lstProducts.ItemsSource = Model.list;
         }
-        public Sepetim(string image,string productName,string price)
-        {
-            InitializeComponent();
-            this.imageSource = image;
-            this.productName = productName;
-            this.price = price;
-            Add();
-            lstProducts.ItemsSource = obList;
-        }
-        ObservableCollection<SubProductItem> obList;
-        public void Add()
-        {
-            obList = new ObservableCollection<SubProductItem>();
-            obList.Add(new SubProductItem
-            {
-                ImageSource = imageSource,
-                Product = productName,
-                Price = price
-            });
-
-        }
+        
 
 
     }
